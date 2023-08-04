5897cbf7d9ccc7bd36a060e15ce3a6de2dea20f5
Mads <mcprem1@hotmail.com>
2023-08-04 15:50:05 +0700

    Finish Part 1

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,25 +1,33 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
-interface Props { 
+interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
-  
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
+        {data?.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,30 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosinstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "66f4cb8d699240f6b16b4e196957a01b",
   },
 });
+
+class APIClient<T> {
+  enpoint: string;
+
+  constructor(enpoint: string) {
+    this.enpoint = enpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosinstance
+      .get<FetchResponse<T>>(this.enpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
