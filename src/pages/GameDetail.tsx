22905afab893e24a93da7209e4bdf828a9027287
Mads <mcprem1@hotmail.com>
2023-08-22 16:16:42 +0700

    finish

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,7 +1,9 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameScreenshots from "../components/GameScreenshots";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetail = () => {
@@ -13,11 +15,17 @@ const GameDetail = () => {
   if (error) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game?.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText>{game?.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game?.id} />\
+        <GameScreenshots gameId={game?.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
